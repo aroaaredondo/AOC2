279d64db54e55b37833b6929d054997da618ab65
aroaaredondo <851769@unizar.es>
2024-02-22 10:29:19 +0100

    ensamblador modificado

--- Practicas/sesion2/851769.asm
+++ Practicas/sesion2/851769.asm
@@ -1,13 +1,12 @@
-MOV r1, #20000    @ Cargar 20000 en r1
-MOV r2, #20000    @ Cargar 20000 en r2
-MOV r3, #10000    @ Cargar 10000 en r3
-MOV r4, #1000     @ Cargar 1000 en r4
-MOV r5, #700      @ Cargar 700 en r5
-MOV r6, #60       @ Cargar 60 en r6
-MOV r7, #9        @ Cargar 9 en r7
+MOV     #20000,  r1         @ Cargar 20000 en r1
+MOV     #20000,  r2         @ Cargar 20000 en r2
+MOV     #10000,  r3         @ Cargar 10000 en r3
+MOV     #1000,   r4         @ Cargar 1000 en r4
+MOV     #700,    r5         @ Cargar 700 en r5
+MOV     #60,     r6         @ Cargar 60 en r6
+MOV     #9,      r7         @ Cargar 9 en r7
 
-ADD r0, r1, r0    @ Sumar r1 a r0
-ADD r0, r2, r0    @ Sumar r2 a r0
+ADD r1, r2, r0    @ Sumar r2 a r0
 ADD r0, r3, r0    @ Sumar r3 a r0
 ADD r0, r4, r0    @ Sumar r4 a r0
 ADD r0, r5, r0    @ Sumar r5 a r0
